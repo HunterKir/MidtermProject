05909af6f9e45b3dc6eca25e9e21d646e6e55ea7
Alex Perez <perezidential@Alexs-MacBook-Pro.local>
2017-11-29 16:52:55 -0700

    community pojo

---
+++ JPAMidtermProject/src/entities/Community.java
@@ -0,0 +1,73 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "community")
+public class Community {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
+	private User owner;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	public String toString() {
+		return "Community [id=" + id + ", name=" + name + ", owner=" + owner + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Community other = (Community) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
