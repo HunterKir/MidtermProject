e97dc2ba56e42e8537482746cd013a1977b53674
Airik Leon <airik.leon@gmail.com>
2017-11-30 09:27:49 -0700

    completed CRUD test cases for User POJO & stubbed CommunityTest

--- JPAMidtermProject/test/test/UserTests.java
+++ JPAMidtermProject/test/test/UserTests.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -29,17 +31,39 @@ public class UserTests {
 		emf.close();
 		dao = null; 
 	}
-
-	@Test
-	public void test_createUser() {
-		User user = new User(); 
-		user.setAdmin(false);
-		user.setFirstName("John");
-		user.setLastName("Doe");
-		user.setPassword("12345");
-		user.setUsername("JohnDOE");
-		
-		
-	}
-
+	/*CRUD test are commented out because they make changes to DB*/
+//	@Test
+//	public void test_createUser() {
+//		User user = new User(); 
+//		user.setAdmin(false);
+//		user.setFirstName("John");
+//		user.setLastName("Doe");
+//		user.setPassword("12345");
+//		user.setUsername("JohnDOE");
+//		
+//		User createdUser = dao.createUser(user);
+//		
+//		assertEquals(createdUser.getFirstName(), "John");
+//		
+//	}
+//	@Test
+//	public void test_updateUser() {
+//		User user = dao.getUser(2); 
+//		user.setFirstName("Allen");
+//		
+//		User updatedUser = dao.updateUser(user.getId(), user);
+//		assertEquals("Allen", updatedUser.getFirstName()); 
+//	}
+//	@Test
+//	public void test_getUser() {
+//		User user = dao.getUser(2);
+//		
+//		assertEquals("Allen", user.getFirstName());
+//	}
+//	@Test
+//	public void test_deleteUser() {
+//		User deletedUser = dao.deleteUser(2); 
+//		
+//		assertEquals("Allen", deletedUser.getFirstName());
+//	}
 }
