f1b3e4d7bfebf10906054c1ff56349b35d588630
Alex Perez <perezidential@Alexs-MacBook-Pro.local>
2017-11-30 08:53:37 -0700

    price,category,descrip filter methods for item dao impl

--- JPAMidtermProject/src/data/ItemDAO.java
+++ JPAMidtermProject/src/data/ItemDAO.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.List;
+
 import entities.Item;
 
 public interface ItemDAO {
@@ -7,4 +9,7 @@ public interface ItemDAO {
 	public Item getItem(int id);
 	public Item deleteItem(int id); 
 	public Item updateItem(int id, Item item); 
+	public List<Item> getItembyCatID(int id);
+	public List<Item> getItembyPrice(double min, double max);
+	public List<Item> getItembyDescription(String descrip);
 }
--- JPAMidtermProject/src/data/ItemDAOImpl.java
+++ JPAMidtermProject/src/data/ItemDAOImpl.java
@@ -1,6 +1,8 @@
 package data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,37 +15,38 @@ import entities.User;
 public class ItemDAOImpl implements ItemDAO {
 	@Override
 	public Item postItem(Item item) {
-		EntityManagerFactory emf = 
-		        Persistence.createEntityManagerFactory("MidtermProject");
-		    EntityManager em = emf.createEntityManager();
-		    
-		    em.getTransaction().begin();
-		    em.persist(item);
-		    em.flush();
-		    em.getTransaction().commit();
-		    em.close();
-		    emf.close();
-		    return null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(item);
+		em.flush();
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		return null;
 	}
+
 	@Override
 	public Item getItem(int id) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
 		EntityManager em = emf.createEntityManager();
-		Item item = null; 
-		item = em.find(Item.class, id);   
+		Item item = null;
+		item = em.find(Item.class, id);
 		return item;
 	}
+
 	@Override
 	public Item deleteItem(int id) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
 		EntityManager em = emf.createEntityManager();
 		Item item = em.find(Item.class, id);
-	    
-	    if(item != null) {
-	    em.getTransaction().begin();
-	    em.remove(item);
-	    em.getTransaction().commit();
-	    }
+
+		if (item != null) {
+			em.getTransaction().begin();
+			em.remove(item);
+			em.getTransaction().commit();
+		}
 
 		return item;
 	}
@@ -52,22 +55,72 @@ public class ItemDAOImpl implements ItemDAO {
 	public Item updateItem(int id, Item item) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("VideoStore");
 		EntityManager em = emf.createEntityManager();
-		
-		   Item itemToUpdate  = em.find(Item.class, id);
-		   em.getTransaction().begin();
-		   
-		   itemToUpdate.setUser(item.getUser());
-		   itemToUpdate.setCategory(item.getCategory());
-		   itemToUpdate.setCommunity(item.getCommunity());
-		   itemToUpdate.setContent(item.getContent());
-		   itemToUpdate.setPosts(item.getPosts());
-		   itemToUpdate.setPostTime(item.getPostTime());
-		   itemToUpdate.setPrice(item.getPrice());
-		   itemToUpdate.setTitle(item.getTitle());		   
-		   em.getTransaction().commit();
-	    em.close();
-	    emf.close();
-	    
-		return itemToUpdate;	
+
+		Item itemToUpdate = em.find(Item.class, id);
+		em.getTransaction().begin();
+
+		itemToUpdate.setUser(item.getUser());
+		itemToUpdate.setCategory(item.getCategory());
+		itemToUpdate.setCommunity(item.getCommunity());
+		itemToUpdate.setContent(item.getContent());
+		itemToUpdate.setPosts(item.getPosts());
+		itemToUpdate.setPostTime(item.getPostTime());
+		itemToUpdate.setPrice(item.getPrice());
+		itemToUpdate.setTitle(item.getTitle());
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+
+		return itemToUpdate;
+	}
+
+	public List<Item> getItembyCatID(int id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
+		EntityManager em = emf.createEntityManager();
+		List<Item> items = new ArrayList<>();
+		try {
+			String q = "SELECT ii from Item ii WHERE ii.category.id =:cid";
+			items = em.createQuery(q, Item.class).setParameter("cid", id).getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		return items;
+	}
+
+	public List<Item> getItembyPrice(double min, double max) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
+		EntityManager em = emf.createEntityManager();
+		List<Item> items = new ArrayList<>();
+		try {
+			String q = "SELECT ii from Item ii WHERE ii.price BETWEEN :min AND :max";
+			items = em.createQuery(q, Item.class).setParameter("min", min).setParameter("max", max).getResultList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		return items;
+	}
+
+	public List<Item> getItembyDescription(String descrip) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("MidtermProject");
+		EntityManager em = emf.createEntityManager();
+		List<Item> items = new ArrayList<>();
+		try {
+			String q = "SELECT ii from Item ii WHERE ii.content LIKE :text";
+			items = em.createQuery(q, Item.class).setParameter("text", "%" + descrip + "%").getResultList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			em.close();
+			emf.close();
+		}
+		return items;
 	}
 }
