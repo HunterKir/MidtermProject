e7691e9417bbffce1acfd583233344b32dc8fb73
Alex Perez <perezidential@Alexs-MacBook-Pro.local>
2017-11-29 18:27:00 -0700

    community dao

--- JPAMidtermProject/src/data/CommunityDAO.java
+++ JPAMidtermProject/src/data/CommunityDAO.java
@@ -3,7 +3,7 @@ package data;
 import entities.Community;
 
 public interface CommunityDAO {
-	public Community getCommunity();
+	public Community getCommunitybyId( int id);
 	public Community createCommunity();
 	public Community updateCommunity();
 	public Community deleteCommunity();
--- JPAMidtermProject/src/data/CommunityDAOImpl.java
+++ JPAMidtermProject/src/data/CommunityDAOImpl.java
@@ -1,12 +1,27 @@
 package data;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import entities.Community;
 
+@Repository
+@Transactional
 public class CommunityDAOImpl implements CommunityDAO{
 
 	@Override
-	public Community getCommunity() {
-		// TODO Auto-generated method stub
+	public Community getCommunitybyId( int id) {
+		Community c = new Community();
+		
+		c=em.
+		
 		return null;
 	}
 
