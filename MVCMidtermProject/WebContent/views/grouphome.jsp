9888488cefc8474fcfef26ad8981d003faf68acc
HunterKir <hunter.kiritsis@gmail.com>
2017-11-30 15:09:01 -0700

    made database changes

--- MVCMidtermProject/WebContent/views/grouphome.jsp
+++ MVCMidtermProject/WebContent/views/grouphome.jsp
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
      <head>
-     <title>Hello, world!</title>
+     <title>${community.name}</title>
          <!-- Required meta tags -->
      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,7 +12,6 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-     <h1>Hello, world!</h1>
 
          <!-- Optional JavaScript -->
          <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- swapmeetdb.sql
+++ swapmeetdb.sql
@@ -1,27 +1,244 @@
--- MySQL dump 10.13  Distrib 5.6.35, for osx10.9 (x86_64)
---
--- Host: localhost    Database: swapmeetdb
--- ------------------------------------------------------
--- Server version	5.6.35
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- -----------------------------------------------------
+-- Schema SwapMeetDB
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `SwapMeetDB` ;
 
--- Dump completed on 2017-11-29 17:32:24
+-- -----------------------------------------------------
+-- Schema SwapMeetDB
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SwapMeetDB` DEFAULT CHARACTER SET utf8 ;
+USE `SwapMeetDB` ;
+
+-- -----------------------------------------------------
+-- Table `community`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `community` ;
+
+CREATE TABLE IF NOT EXISTS `community` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `owner_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC),
+  INDEX `id_idx` (`owner_id` ASC),
+  CONSTRAINT `user_id_fk`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `admin` TINYINT NULL,
+  `home_community_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `community_home_id_fk_idx` (`home_community_id` ASC),
+  CONSTRAINT `community_home_id_fk`
+    FOREIGN KEY (`home_community_id`)
+    REFERENCES `community` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `category` ;
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `item`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `item` ;
+
+CREATE TABLE IF NOT EXISTS `item` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  `post_time` TIMESTAMP NOT NULL,
+  `category_id` INT NULL,
+  `price` DECIMAL NULL,
+  `title` TEXT NOT NULL,
+  `community_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`user_id` ASC),
+  INDEX `category_id_idx` (`category_id` ASC),
+  INDEX `community_id_idx` (`community_id` ASC),
+  CONSTRAINT `user_item_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `category_item_fk`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `community_item_fk`
+    FOREIGN KEY (`community_id`)
+    REFERENCES `community` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post` ;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `item_id` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  `post_time` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`item_id` ASC),
+  INDEX `id_idx1` (`user_id` ASC),
+  CONSTRAINT `post_item_fk`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `item` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `post_user_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_community`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_community` ;
+
+CREATE TABLE IF NOT EXISTS `user_community` (
+  `user_id` INT NOT NULL,
+  `community_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `community_id`),
+  INDEX `fk_user_has_community_community1_idx` (`community_id` ASC),
+  INDEX `fk_user_has_community_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_community_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_community_community1`
+    FOREIGN KEY (`community_id`)
+    REFERENCES `community` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_rating` ;
+
+CREATE TABLE IF NOT EXISTS `user_rating` (
+  `community_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `rating` INT NOT NULL,
+  INDEX `user_rating_fk_idx` (`user_id` ASC),
+  CONSTRAINT `user_rating_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user_community` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `community_rating_fk`
+    FOREIGN KEY (`community_id`)
+    REFERENCES `user_community` (`community_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO blossom@localhost;
+ DROP USER blossom@localhost;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE USER 'blossom'@'localhost' IDENTIFIED BY 'blossom';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'blossom'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+-- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `SwapMeetDB`;
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `admin`, `home_community_id`) VALUES (1, 'Robert', 'Smith', 'bobsmith', 'bobbob', NULL, NULL);
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `username`, `password`, `admin`, `home_community_id`) VALUES (2, 'Hunter', 'Kiritsis', 'HunterK', 'password123', 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `community`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `SwapMeetDB`;
+INSERT INTO `community` (`id`, `name`, `owner_id`) VALUES (1, 'Gobots', 1);
+INSERT INTO `community` (`id`, `name`, `owner_id`) VALUES (2, 'SD13', 2);
+
+COMMIT;
+
+
+
+-- -----------------------------------------------------
+-- Data for table `category`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `SwapMeetDB`;
+INSERT INTO `category` (`id`, `type`) VALUES (1, 'Vehicles');
+INSERT INTO `category` (`id`, `type`) VALUES (2, 'Toys and Games');
+INSERT INTO `category` (`id`, `type`) VALUES (3, 'Sports Equipment');
+INSERT INTO `category` (`id`, `type`) VALUES (4, 'Furniture');
+INSERT INTO `category` (`id`, `type`) VALUES (5, 'Tickets');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `item`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `SwapMeetDB`;
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (1, 1, 'A chair', '1970-01-01 00:00:01', 1, 1, 'Chair', 1);
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (2, 1, 'A squid', '1970-01-01 00:00:01', 2, 1, 'Squid', 1);
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (3, 1, 'A bowl', '1970-01-01 00:00:01', 2, 1, 'Bowl', 1);
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (4, 1, 'A Car', '1970-01-01 00:00:01', 3, 1, 'Car', 1);
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (5, 1, 'A bird', '1970-01-01 00:00:01', 3, 1, 'Bird', 1);
+INSERT INTO `item` (`id`, `user_id`, `content`, `post_time`, `category_id`, `price`, `title`, `community_id`) VALUES (6, 2, 'Coffee Beans', '2017-12-01', 2, NULL, 'Free Coffee', 2);
+
+COMMIT;
